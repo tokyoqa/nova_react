785c56c32db44e04c6c573bc225a706106cb1c13
tokyoqa <daniyar.bolotbekov@inbox.ru>
2022-03-09 00:45:02 +0300

    camera

--- src/Components/Camera/Camera.js
+++ src/Components/Camera/Camera.js
@@ -1,19 +1,66 @@
-import React, { useRef, useState } from 'react';
-import './Camera.css'
+import React, { Component } from 'react';
+import Camera from 'react-camera';
 
+export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.takePicture = this.takePicture.bind(this);
+  }
 
+  takePicture() {
+    this.camera.capture()
+    .then(blob => {
+      this.img.src = URL.createObjectURL(blob);
+      this.img.onload = () => { URL.revokeObjectURL(this.src); }
+    })
+  }
 
-
-
-const Component = () => {
-
-  return (
-    <div>
-
-    </div>
-  );
+  render() {
+    return (
+      <div style={style.container}>
+        <Camera
+          style={style.preview}
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+        >
+          <div style={style.captureContainer} onClick={this.takePicture}>
+            <div style={style.captureButton} />
+          </div>
+        </Camera>
+        <img
+          style={style.captureImage}
+          ref={(img) => {
+            this.img = img;
+          }}
+        />
+      </div>
+    );
+  }
 }
 
-
-export default Camera;
+const style = {
+  preview: {
+    position: 'relative',
+  },
+  captureContainer: {
+    display: 'flex',
+    position: 'absolute',
+    justifyContent: 'center',
+    zIndex: 1,
+    bottom: 0,
+    width: '100%'
+  },
+  captureButton: {
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    height: 56,
+    width: 56,
+    color: '#000',
+    margin: 20
+  },
+  captureImage: {
+    width: '100%',
+  }
+};
--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "./Video.css"
-
 function Video(){
     return(
     <div className="video_form">
          <div className="video_title">Идентификация</div>
             <div className="video_subtitle">Отправьте свое видео</div>
-            <div className="video_
-            
-            .....
-            input-container ic1">
-            </div>
+            <div className="video"> </div>
         <button type="text" className="video_submit">Далее</button>
     </div>
     )
