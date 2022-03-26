619abc97ec0126b58daff8375a82ef2e60098a18
tokyoqa <daniyar.bolotbekov@inbox.ru>
2022-03-26 17:38:39 +0600

    camera fixed

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -38,7 +38,6 @@ const Main = ({setId}) => {
   ) 
   .then((res) => { setId(res.data.id);
     if (res.data.statusCode = 2){
-      <Response/>
     } 
     navigate('/identification')
     console.log(res)
